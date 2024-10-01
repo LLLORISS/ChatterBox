8257162fe5430d51cb8659347bc8e8769600f5e4
Nazar Muts <nazikmuz@gmail.com>
2024-10-01 23:50:05 +0300

    First commit

--- src/main/java/org/example/chatterbox/HelloApplication.java
+++ src/main/java/org/example/chatterbox/HelloApplication.java
@@ -3,17 +3,20 @@ package org.example.chatterbox;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) {
+        Button button = new Button("Click Me!");
+        button.setOnAction(e -> System.out.println("Button Clicked!"));
+
+        Scene scene = new Scene(button, 300, 200);
         stage.setScene(scene);
+        stage.setTitle("JavaFX Test");
         stage.show();
     }
 
